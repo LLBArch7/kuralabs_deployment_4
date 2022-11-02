ba56d0fdde869daba98dafa4d9d78a00dbb4bfab
Lora Lee <107732101+LLBArch7@users.noreply.github.com>
2022-11-02 16:15:46 -0400

    Added basic code for VPC resource.

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -18,8 +18,11 @@ resource "aws_instance" "web_server01" {
 
   tags = {
     "Name" : "Webserver001"
-  }
-  
+  } 
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
 output "instance_ip" {
